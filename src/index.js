7f5015d8455c884c8a7b958408f0950f1cea1a5a
Gregory Kravchenko <2crazymail@gmail.com>
2019-09-23 23:57:19 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let open = [],
+        close = [];
+
+    bracketsConfig.forEach(function(pair) {
+        open.push(pair[0]);
+        close.push(pair[1]);
+    })
+
+    let opened = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if ( str[i] == close[open.indexOf(opened[opened.length - 1])] ) {
+            opened.pop();
+        } else if (open.includes(str[i])) {
+            opened.push(str[i]);
+        } else {
+            return false;
+        };
+    }
+    return opened.length == 0;
 }
